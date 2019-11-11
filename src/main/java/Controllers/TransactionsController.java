ef5662e3c7f26f2d94e0684a0234bf4229c7f351
themanofthehour200 <87534@farnborough.ac.uk>
2019-11-11 10:27:55 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/libraries/Maven__com_googlecode_json_simple_json_simple_1_1_1.xml
    #	.idea/libraries/Maven__com_sun_istack_istack_commons_runtime_3_0_8.xml
    #	.idea/libraries/Maven__com_sun_jersey_contribs_jersey_multipart_1_19_4.xml
    #	.idea/libraries/Maven__com_sun_jersey_jersey_core_1_19_4.xml
    #	.idea/libraries/Maven__com_sun_xml_bind_jaxb_impl_2_3_2.xml
    #	.idea/libraries/Maven__com_sun_xml_fastinfoset_FastInfoset_1_2_16.xml
    #	.idea/libraries/Maven__jakarta_activation_jakarta_activation_api_1_2_1.xml
    #	.idea/libraries/Maven__jakarta_annotation_jakarta_annotation_api_1_3_4.xml
    #	.idea/libraries/Maven__jakarta_ws_rs_jakarta_ws_rs_api_2_1_5.xml
    #	.idea/libraries/Maven__jakarta_xml_bind_jakarta_xml_bind_api_2_3_2.xml
    #	.idea/libraries/Maven__javax_activation_javax_activation_api_1_2_0.xml
    #	.idea/libraries/Maven__javax_servlet_javax_servlet_api_3_1_0.xml
    #	.idea/libraries/Maven__javax_validation_validation_api_2_0_1_Final.xml
    #	.idea/libraries/Maven__javax_ws_rs_jsr311_api_1_1_1.xml
    #	.idea/libraries/Maven__javax_xml_bind_jaxb_api_2_3_1.xml
    #	.idea/libraries/Maven__junit_junit_4_10.xml
    #	.idea/libraries/Maven__org_eclipse_jetty_jetty_continuation_9_4_12_v20180830.xml
    #	.idea/libraries/Maven__org_eclipse_jetty_jetty_http_9_4_18_v20190429.xml
    #	.idea/libraries/Maven__org_eclipse_jetty_jetty_io_9_4_18_v20190429.xml
    #	.idea/libraries/Maven__org_eclipse_jetty_jetty_security_9_4_18_v20190429.xml
    #	.idea/libraries/Maven__org_eclipse_jetty_jetty_server_9_4_18_v20190429.xml
    #	.idea/libraries/Maven__org_eclipse_jetty_jetty_servlet_9_4_18_v20190429.xml
    #	.idea/libraries/Maven__org_eclipse_jetty_jetty_util_9_4_18_v20190429.xml
    #	.idea/libraries/Maven__org_glassfish_hk2_external_aopalliance_repackaged_2_5_0.xml
    #	.idea/libraries/Maven__org_glassfish_hk2_external_jakarta_inject_2_5_0.xml
    #	.idea/libraries/Maven__org_glassfish_hk2_hk2_api_2_5_0.xml
    #	.idea/libraries/Maven__org_glassfish_hk2_hk2_locator_2_5_0.xml
    #	.idea/libraries/Maven__org_glassfish_hk2_hk2_utils_2_5_0.xml
    #	.idea/libraries/Maven__org_glassfish_hk2_osgi_resource_locator_1_0_1.xml
    #	.idea/libraries/Maven__org_glassfish_jaxb_jaxb_runtime_2_3_2.xml
    #	.idea/libraries/Maven__org_glassfish_jaxb_txw2_2_3_2.xml
    #	.idea/libraries/Maven__org_glassfish_jersey_containers_jersey_container_jetty_http_2_28.xml
    #	.idea/libraries/Maven__org_glassfish_jersey_containers_jersey_container_servlet_core_2_28.xml
    #	.idea/libraries/Maven__org_glassfish_jersey_core_jersey_client_2_28.xml
    #	.idea/libraries/Maven__org_glassfish_jersey_core_jersey_common_2_28.xml
    #	.idea/libraries/Maven__org_glassfish_jersey_core_jersey_server_2_28.xml
    #	.idea/libraries/Maven__org_glassfish_jersey_inject_jersey_hk2_2_28.xml
    #	.idea/libraries/Maven__org_glassfish_jersey_media_jersey_media_jaxb_2_28.xml
    #	.idea/libraries/Maven__org_glassfish_jersey_media_jersey_media_multipart_2_28.xml
    #	.idea/libraries/Maven__org_hamcrest_hamcrest_core_1_1.xml
    #	.idea/libraries/Maven__org_javassist_javassist_3_22_0_CR2.xml
    #	.idea/libraries/Maven__org_jvnet_mimepull_mimepull_1_9_11.xml
    #	.idea/libraries/Maven__org_jvnet_staxex_stax_ex_1_8_1.xml
    #	resources/courseworkDatabase.db

--- src/main/java/Controllers/TransactionsController.java
+++ src/main/java/Controllers/TransactionsController.java
@@ -1,7 +1,12 @@
 package Controllers;
 
 import Server.main;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,10 +15,133 @@ import java.util.List;
 
 import static java.lang.System.out;
 
+@Path("Transactions/")
 public class TransactionsController{
 
-    //This is the method for selecting all rows in the table of Users
-    //This method is mainly just used for testing purposes, as this is easier than manually having to check the Accounts table after each applicable test
+    @POST
+    @Path("create")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+
+    public String insert(@FormDataParam("categoryID") int categoryID, @FormDataParam("date") String date, @FormDataParam("accountID") int accountID, @FormDataParam("balanceChange") int balanceChange,
+                         @FormDataParam("description") String description, @FormDataParam("standingOrderID") int standingOrderID){
+
+        try{
+            PreparedStatement ps = main.db.prepareStatement("INSERT INTO Transactions (TransactionID, AccountID, BalanceChange, CategoryID," +
+                    " Description, Date, StandingOrderID) VALUES (?,?,?,?,?,?,?)"); //This adds the transaction to the transaction database
+
+            //makes sure that a transaction with a value of zero isn't done, as this is pointless and may lead to errors
+            if (balanceChange == 0) throw new Exception("Transaction cannot be for a value of 0");
+
+            out.println("Transactions/create");
+
+            ps.setString(1,null);//auto-increments the primary key
+            fillColumn(accountID,balanceChange,categoryID,description,date,standingOrderID,ps,1);
+
+            ps.executeUpdate();
+
+            //This gets the account balance for the account the transaction is relating to
+            PreparedStatement ps2 = main.db.prepareStatement("SELECT Balance FROM Accounts WHERE AccountID = ?");
+
+            JSONObject item = new JSONObject();
+
+            ps2.setInt(1,accountID); //The user with the specific account ID is searched for
+            ResultSet result = ps2.executeQuery();
+
+            //This updates the account balance to reflect the transaction
+
+            PreparedStatement ps3 =  main.db.prepareStatement("UPDATE Accounts SET Balance = ? WHERE AccountID = ?");
+            ps3.setInt(1,(result.getInt(1)-balanceChange));
+            ps3.setInt(2,accountID);
+            ps3.executeUpdate();
+            return "{\"status\": \"OK\"}";
+
+        } catch (Exception e){
+            out.println("Error when inputting user into database, error code\n" + e.getMessage());
+            return "{\"error\": \"Unable to create new item, please see server console for more info.\"}";
+        }
+    }
+
+    @GET
+    @Path("view/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+
+    //This returns a specific user's details
+    public String search(@PathParam("id") Integer searchID){
+        try {
+            if (searchID == null) {
+                throw new Exception("Thing's 'id' is missing in the HTTP request's URL.");
+            }
+
+            System.out.println("Transactions/view/" + searchID);
+            JSONArray list = new JSONArray();
+
+
+            PreparedStatement ps = main.db.prepareStatement("SELECT * FROM Transactions WHERE AccountID = ?");
+
+            ps.setInt(1,searchID); //The user with the specific account ID is searched for
+
+            ResultSet result = ps.executeQuery();
+
+            while (result.next()) {
+                JSONObject item = new JSONObject();
+                item.put("TransactionID", result.getInt(1));
+                item.put("AccountID", result.getInt(2));
+                item.put("BalanceChange", result.getInt(3));
+                item.put("CategoryID", result.getInt(4));
+                item.put("Description", result.getString(5));
+                item.put("Date", result.getString(6));
+                item.put("StandingOrderID", result.getInt(7));
+                list.add(item);
+            }
+            return list.toString();
+
+        }
+        catch (Exception e){
+            out.println("Error searching database 'Transactions', error message:\n" + e.getMessage());
+            return "{\"error\": \"Unable to get item, please see server console for more info.\"}";
+
+        }
+    }
+
+    @POST
+    @Path("search")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)//Jersey turns this into an HTTP request handler
+    public String search(@FormDataParam("categoryID") int categoryID, @FormDataParam("date") String date, @FormDataParam("accountID") int accountID){
+
+        System.out.println("Transactions/search");
+        JSONArray list = new JSONArray();
+
+        try{
+            PreparedStatement ps = main.db.prepareStatement("SELECT * FROM Transactions WHERE AccountID = ? AND Date = ? OR CategoryID = ?");
+            ps.setInt(1,accountID);
+            ps.setString(2,date);
+            ps.setInt(3,categoryID);
+            ResultSet result = ps.executeQuery();
+
+            boolean added = false;//This determines whether any transactions with the search criteria have been found
+            while (result.next()) {
+                added = true;
+                JSONObject item = new JSONObject();
+                item.put("TransactionID", result.getInt(1));
+                item.put("AccountID", result.getInt(2));
+                item.put("BalanceChange", result.getInt(3));
+                item.put("CategoryID", result.getInt(4));
+                item.put("Description", result.getString(5));
+                item.put("Date", result.getString(6));
+                item.put("StandingOrderID", result.getInt(7));
+                list.add(item);
+            }
+            if (added) return list.toString(); //If transactions found then return them
+            else throw new Exception("No transactions with search criteria found"); //if none have been found then give an error
+
+        } catch (Exception e){
+            System.out.println("Database error: " + e.getMessage());
+            return "{\"error\": \"Unable to list items, please see server console for more info.\"}";
+        }
+    }
+
     public static List selectAll(){
         try{
             PreparedStatement ps = main.db.prepareStatement("SELECT * FROM Transactions");
