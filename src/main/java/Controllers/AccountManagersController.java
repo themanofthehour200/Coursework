cbcf18638eed7367f96fa51954da5f39d086e642
themanofthehour <adamsbrown.906@gmail.com>
2019-11-14 23:27:03 +0000

    ALL API CALLS DONE :)

--- src/main/java/Controllers/AccountManagersController.java
+++ src/main/java/Controllers/AccountManagersController.java
@@ -1,7 +1,12 @@
 package Controllers;
 
 import Server.main;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,101 +15,121 @@ import java.util.List;
 
 import static java.lang.System.out;
 
+@Path("AccountManagers/")
 public class AccountManagersController{
 
-    //This is the method for selecting all rows in the table of Users
-    //This method is mainly just used for testing purposes, as this is easier than manually having to check the Accounts table after each applicable test
-    public static List selectAll(){
-        try{
-            PreparedStatement ps = main.db.prepareStatement("SELECT * FROM AccountManagers");
-            ResultSet result = ps.executeQuery();
+    //This method returns all the managers for an account
+    @GET
+    @Path("list/{accountID}")
+    @Produces(MediaType.APPLICATION_JSON)
 
-            int count = 0;
-            List<List<String>> output = new ArrayList<List<String>>(); //This is a List of ArrayLists. This is what is returned.
-            //An ArrayList is used instead of an array as it is mutatable and we don't know how many rows there are in the table
+    public String search(@PathParam("accountID") Integer searchID){
+        System.out.println("AccountManagers/list/" + searchID);
+
+        try{
+            if (searchID == null) throw new Exception("No account exists");
+            JSONArray list = new JSONArray();
+
+            PreparedStatement ps = main.db.prepareStatement("SELECT * FROM AccountManagers WHERE AccountID = ?");
+            ps.setInt(1,searchID);
+            ResultSet result = ps.executeQuery();
 
             while(result.next()){
-                output.add(new ArrayList<String>());            //A new arraylist is created within the overall output List
-                output.get(count).add(Integer.toString(result.getInt(1)));      //The value is added in to the current ArrayList within output
-                output.get(count).add(Integer.toString(result.getInt(2)));
-                output.get(count).add(Integer.toString(result.getInt(3)));
-                output.get(count).add(result.getString(result.getInt(4)));
-
-                out.println(output.get(count)); //To be removed once testing phase one is done
-                count++;
+                JSONObject item = new JSONObject();
+                item.put("ControlID", result.getInt(1));
+                item.put("AccountID", result.getInt(2));
+                item.put("ManagerID", result.getInt(3));
+                item.put("AccessLevel", result.getInt(4));
+                list.add(item);
             }
-            return output;
+            return list.toString();
 
         } catch (Exception e){
-            out.println("Error reading database 'AccountManagers', error message:\n" + e.getMessage());
-            return null;
+            System.out.println("Database error: " + e.getMessage());
+            return "{\"error\": \"Unable to list items, please see server console for more info.\"}";
         }
     }
 
-    //This returns a specific accounts details, allowing the user to check their balance etc.
-    public static List search(int searchID, int accountID){
-        try {
-            PreparedStatement ps = main.db.prepareStatement("SELECT * FROM AccountManagers WHERE ControlID = ? OR AccountID = ?");
-            ps.setInt(1,searchID); //The user with the specific account ID is searched for
-            ps.setInt(2,accountID);
+    @POST
+    @Path("new")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+
+    public String insert(@FormDataParam("accountID") int accountID, @FormDataParam("managerID") int managerID, @FormDataParam("AccessLevel") int accessLevel){
+
+        try{
+            out.println("/AccountManagers/new");
+
+            //This is to ensure that no one account has the same user being a manager on it twice
+            PreparedStatement ps = main.db.prepareStatement("SELECT * FROM AccountManagers WHERE AccountID = ? AND ManagerID = ?");
+            ps.setInt(1,accountID);
+            ps.setInt(2,managerID);
             ResultSet result = ps.executeQuery();
+            if(result.next()) throw new Exception("User is already a manager");
 
-            ArrayList<String> output = new ArrayList<String>(1);
-            output.add(Integer.toString(result.getInt(1)));      //The value is added in to the current ArrayList within output
-            output.add(Integer.toString(result.getInt(2)));
-            output.add(Integer.toString(result.getInt(3)));
-            output.add(result.getString(result.getInt(4)));
-            out.println(output);
-            return output;
+            //Preparing to insert the new manager into the database
+            PreparedStatement ps2 = main.db.prepareStatement("INSERT INTO AccountManagers (ControlID, AccountID, ManagerID, AccessLevel) VALUES (?,?,?,?)");
 
-        }
-        catch (Exception e){
-            out.println("Error searching database 'AccountManagers', error message:\n" + e.getMessage());
-            return null;
-        }
 
-    }
-
-    public static void insert(int accountID, int managerID, int accessLevel){
-
-        try{
-            PreparedStatement ps = main.db.prepareStatement("INSERT INTO AccountManagers (ControlID, AccountID, ManagerID, AccessLevel) VALUES (?,?,?,?)");
-
-            ps.setString(1,null);
-            fillColumn(accountID, managerID, accessLevel, ps,1);
-            ps.executeUpdate();
+            ps2.setString(1,null);//auto-increments the primary key
+            fillColumn(accountID,managerID,accessLevel,ps2,1);
+            ps2.executeUpdate();
+            return "{\"status\": \"OK\"}";
 
         } catch (Exception e){
-            out.println("Error when inputting transaction into database, error code\n" + e.getMessage());
+            out.println("Error when inputting manager into database, error code\n" + e.getMessage());
+            return "{\"error\": \"Unable to create new item, please see server console for more info.\"}";
         }
     }
 
-    public static void delete(int searchID){
+
+    @POST
+    @Path("edit")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String update(@FormDataParam("controlID") int controlID, @FormDataParam("accountID") int accountID, @FormDataParam("managerID") int managerID, @FormDataParam("AccessLevel") int accessLevel){
         try{
+
+            System.out.println("AccountManagers/edit id = " + controlID);
+
+            PreparedStatement ps = main.db.prepareStatement("UPDATE AccountManagers SET AccountID = ?, ManagerID = ?, AccessLevel = ? WHERE ControlID = ?");
+            fillColumn(accountID,managerID,accessLevel, ps,0);
+            ps.setInt(4,controlID);
+            ps.executeUpdate();
+            return "{\"status\": \"OK\"}";
+
+        } catch (Exception e){
+            out.println("Error updating Account Manager, error message:\n" + e.getMessage());
+            return "{\"error\": \"Unable to update item, please see server console for more info.\"}";
+        }
+    }
+
+    @POST
+    @Path("delete")
+    /*The reason that this uses form data instead of having the data be sent via the API path in the
+      path name is that the form is more secure*/
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String delete(@FormDataParam("controlID") Integer searchID){
+        try{
+            //This throws when there has been no controlID entered
+            if (searchID == null) throw new Exception("One or more form data parameters are missing in the HTTP request.");
+
+            out.println("AccountManagers/delete id = " + searchID);
+
             PreparedStatement ps = main.db.prepareStatement("DELETE FROM AccountManagers WHERE ControlID = ?");
             ps.setInt(1,searchID);
             ps.execute();
 
-            out.println("Control number " + searchID + " was deleted successfully");
+            return "{\"status\": \"OK\"}";
 
         } catch (Exception e){
-            out.println("Error deleting control, error message:\n" + e.getMessage());
+            out.println("Error deleting Manager, error message:\n" + e.getMessage());
+            return "{\"error\": \"Unable to delete item, please see server console for more info.\"}";
         }
     }
 
-    public static void update(int controlID, int accountID, int managerID, int accessLevel){
-        try{
-            PreparedStatement ps = main.db.prepareStatement("UPDATE AccountManagers SET AccountID = ?, ManagerID = ?, AccessLevel = ? WHERE ControlID = ?");
 
-
-            fillColumn(accountID, managerID, accessLevel, ps,0);
-            ps.setInt(4,controlID);
-            ps.executeUpdate();
-
-        } catch (Exception e){
-            out.println("Error updating control, error message:\n" + e.getMessage());
-        }
-    }
 
     /* removes the duplicate code of the data entry into the SQL statement for update() and add(), as there code was very similar */
     private static void fillColumn(int accountID, int managerID, int accessLevel, PreparedStatement ps, int column) throws SQLException {
