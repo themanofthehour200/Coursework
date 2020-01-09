b4aa6b961cb1d8548a20d63538a9cb184dd37826
themanofthehour200 <87534@farnborough.ac.uk>
2020-01-09 14:03:14 +0000

    tiny change

--- resources/client/js/signup.js
+++ resources/client/js/signup.js
@@ -2,23 +2,24 @@ function pageLoad() {
 
     document.getElementById("signupButton").addEventListener("click", signup);
 
-    function signup(event){
-        console.log("Sign up function");
+}
 
-        event.preventDefault(); //cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur.
+function signup(event){
+    console.log("Sign up function");
 
-        const form = document.getElementById("signupForm");
-        const formData = new FormData(form);
+    event.preventDefault(); //cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur.
 
-        fetch("/Users/new", {method: 'post', body: formData}
-        ).then(response => response.json()
-        ).then(responseData => {
-            if (responseData.hasOwnProperty('error')) {
-                alert(responseData.error);
-            } else {
-                alert("User account created!\n Returning to log in page")
-                window.location.href = '/client/index.html';
-            }
-        });
-    }
+    const form = document.getElementById("signupForm");
+    const formData = new FormData(form);
+
+    fetch("/Users/new", {method: 'post', body: formData}
+    ).then(response => response.json()
+    ).then(responseData => {
+        if (responseData.hasOwnProperty('error')) {
+            alert(responseData.error);
+        } else {
+            alert("User account created!\n Returning to log in page")
+            window.location.href = '/client/index.html';
+        }
+    });
 }
